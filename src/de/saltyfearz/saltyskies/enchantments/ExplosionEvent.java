3d88eda4634c0619eea573da62f1fdbdee935834
jeiletz <Justin.Eiletz@x-cell.com>
2020-08-31 14:08:02 +0200

    Mediccommand, Enchantments added

--- src/de/saltyfearz/saltyskies/enchantments/ExplosionEvent.java
+++ src/de/saltyfearz/saltyskies/enchantments/ExplosionEvent.java
@@ -2,6 +2,7 @@ package de.saltyfearz.saltyskies.enchantments;
 
 import de.saltyfearz.saltyskies.SaltySkies;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,7 +28,16 @@ public class ExplosionEvent implements Listener {
 
         level = iS.getEnchantments().get( "EXPLOSION" );
 
+        if ( Math.random() > ( 0.1 * level ) ) return;
 
+        LivingEntity victim = ( LivingEntity ) event.getEntity();
 
+        boolean maxLevel = CustomEnchantments.EXPLOSION.getMaxLevel() == level;
+
+        player.getWorld().createExplosion( event.getEntity().getLocation(), level, maxLevel );
+
+        player.sendMessage( plugin.getMsgDE().getMessageInfoDE( "enchant-command", "explosion" ) );
+
+        if ( victim instanceof Player ) victim.sendMessage( plugin.getMsgDE().getMessageInfoDE( "enchant-command", "getExplosion" ) );
     }
 }
