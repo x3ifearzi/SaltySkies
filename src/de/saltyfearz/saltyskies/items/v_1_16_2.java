9ef522c29ed4da1c9d98b6a3d0950005b45024cc
jeiletz <Justin.Eiletz@x-cell.com>
2020-10-23 17:46:00 +0200

    starting itemmethods

--- src/de/saltyfearz/saltyskies/items/v_1_16_2.java
+++ src/de/saltyfearz/saltyskies/items/v_1_16_2.java
@@ -1,6 +1,7 @@
 package de.saltyfearz.saltyskies.items;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -10,5 +11,35 @@ import java.util.List;
 import java.util.Objects;
 
 public class v_1_16_2 {
+
+    public ItemStack item( final Material material, final String displayName, final String singleLore ) {
+
+        final ItemStack iS = new ItemStack( material );
+
+        final ItemMeta iM = iS.getItemMeta();
+
+        if ( iM == null ) return null;
+
+        iM.setDisplayName( displayName );
+
+        if ( singleLore != null ) {
+
+            iM.setLore( ( singleLore.isEmpty() ? new ArrayList<>() : Arrays.asList( singleLore.split( "\\n" ) ) ) );
+
+        }
+
+        this.addItemFlags( iM );
+
+        iS.setItemMeta( iM );
+
+        return iS;
+
+    }
+
+    public void addItemFlags( final ItemMeta iM ) {
+
+        iM.addItemFlags( ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_DESTROYS, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_PLACED_ON, ItemFlag.HIDE_POTION_EFFECTS, ItemFlag.HIDE_UNBREAKABLE);
+
+    }
 
 }
